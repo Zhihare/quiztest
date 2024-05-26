c0c70820b17072c003ec49b08fe3e83dd5fc066c
Zhihare <vladislav.zhihar@gmail.com>
2024-05-26 20:50:22 +0300

    Update CalculateScore.ts

--- src/components/Quiz/CalculateScore.ts
+++ src/components/Quiz/CalculateScore.ts
@@ -1,13 +1,13 @@
-
-import { Quiz} from '../../redax/types';
+import { Quiz } from '../../redax/types';
 
 const calculateScore = (
   quiz: Quiz,
-    selectedAnswers: Map<number, Set<number>>,
-  score: number|undefined
+  selectedAnswers: Map<number, Set<number>>,
+  score: number
 ): number => {
   let totalScore = 0;
-    const scoreAnswers = score ? score : 1;
+  const scoreAnswers = score ? score : 1;
+  
   quiz.questions.forEach((question, questionIndex) => {
     const correctAnswers = question.answers
       .map((answer, answerIndex) => (answer.isCorrect ? answerIndex : null))
@@ -16,20 +16,25 @@ const calculateScore = (
     const selectedAnswersForQuestion = Array.from(
       selectedAnswers.get(questionIndex) || new Set<number>()
     );
-
+   
     const correctAnswersSet = new Set(correctAnswers);
-
     let questionScore = 0;
+
+    const correctSelectedCount = selectedAnswersForQuestion.filter((ans) =>
+      correctAnswersSet.has(ans)
+    ).length;
+
+    const incorrectSelectedCount = selectedAnswersForQuestion.length - correctSelectedCount;
 
     if (arraysEqual(correctAnswers, selectedAnswersForQuestion)) {
       questionScore = scoreAnswers;
-    } else if (
-      selectedAnswersForQuestion.some((ans) => correctAnswersSet.has(ans))
-    ) {
-      const partialCorrectCount = selectedAnswersForQuestion.filter((ans) =>
-        correctAnswersSet.has(ans)
-      ).length;
-      questionScore = (partialCorrectCount*scoreAnswers) / correctAnswers.length;
+    } else if (correctSelectedCount > 0 && incorrectSelectedCount === 0) {
+      
+      questionScore = (correctSelectedCount / correctAnswers.length) * scoreAnswers;
+    } else if (correctSelectedCount > 0 && incorrectSelectedCount > 0) {
+      
+      questionScore = ((correctSelectedCount / correctAnswers.length) - (incorrectSelectedCount / question.answers.length)) * scoreAnswers;
+      if (questionScore < 0) questionScore = 0; 
     }
 
     totalScore += questionScore;
