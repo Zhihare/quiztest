21dcc3fdb99656f5c60a05b7736514d6154704c3
Zhihare <vladislav.zhihar@gmail.com>
2024-05-29 16:08:37 +0300

    width 6xl

--- src/components/QuizList/QuizList.tsx
+++ src/components/QuizList/QuizList.tsx
@@ -38,7 +38,7 @@ const QuizList: React.FC = () => {
   );
 
 return(
-   <div className="max-w-4xl mx-auto p-4 bg-white shadow-md rounded-lg">
+   <div className="max-w-6xl mx-auto p-4 bg-white shadow-md rounded-lg">
       <h2 className="text-2xl font-bold mb-4">Quiz List</h2>
       <button onClick={() => navigate(`/form/${generateId()}`)} className="px-4 py-2 bg-blue-500 text-white rounded mb-4">
         Add New Quiz
@@ -51,7 +51,7 @@ return(
           onChange={(e) => setSearchTerm(e.target.value)}
           className="w-full border-2 border-gray-200 p-2 rounded mb-4"
         />
-       <ul className="grid grid-cols-1 md:grid-cols-2 gap-4">
+       <ul className="grid grid-cols-1 md:grid-cols-3 gap-4">
         {filteredQuizzes.map(quiz => (
           <li key={quiz.id} className="flex flex-col justify-between h-48 p-4 border-4 rounded-lg" style={{ background: quiz.color }}>
             <div className="flex-grow">
